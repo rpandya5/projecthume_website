029371e79c70b225a6858497921fba7eac7fdc53
rpandya5 <richapandya273@gmail.com>
2024-06-08 20:07:27 -0400

    updated small popups

--- src/components/OrangeWindow.js
+++ src/components/OrangeWindow.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const OrangeWindow = () => (
-    <div style={{ backgroundColor: 'rgb(242, 123, 24)' }} className="p-2 rounded-lg border-8 border-white shadow-lg"> {/* Reduced padding from p-4 to p-2 */}
+    <div style={{ backgroundColor: 'rgb(242, 123, 24)' }} className="green-spage p-2 rounded-lg border-8 border-white shadow-lg"> {/* Reduced padding from p-4 to p-2 */}
         <div style={{ backgroundColor: 'rgb(242, 123, 24)' }} className="p-2 rounded-lg"> {/* Reduced padding from p-4 to p-2 */}
             <h2 className="text-black title-font text-xl text-center">Intraoral Interfaces</h2> {/* Reduced font size from text-2xl to text-xl */}
-            <p className="body-text mt-2 text-black text-center">This one's still cooking...</p> {/* Reduced margin top from mt-4 to mt-2 */}
+            <p className="body-text-bold mt-2 text-black text-left">
+                Status: <span className="body-text">In progress</span>
+            </p>
+            <p className="body-text mt-2 text-black text-left">
+                A smart retainer to control my computer without my hands.
+            </p>
+            <button className="expand-btn body-text-bold mt-2 text-black text-center">[ expand ]</button>
         </div>
     </div>
 );
--- src/components/PurpleWindow.js
+++ src/components/PurpleWindow.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const PurpleWindow = () => (
-    <div style={{ backgroundColor: 'rgb(161, 184, 248)' }} className="p-2 rounded-lg border-8 border-white shadow-lg"> {/* Reduced padding from p-4 to p-2 */}
+    <div style={{ backgroundColor: 'rgb(161, 184, 248)' }} className="green-spage p-2 rounded-lg border-8 border-white shadow-lg"> {/* Reduced padding from p-4 to p-2 */}
         <div style={{ backgroundColor: 'rgb(161, 184, 248)' }} className="p-2 rounded-lg"> {/* Reduced padding from p-4 to p-2 */}
             <h2 className="text-black title-font text-xl text-center">On Skin Interfaces</h2> {/* Reduced font size from text-2xl to text-xl */}
-            <p className="body-text mt-2 text-black text-center">This one's still cooking...</p> {/* Reduced margin top from mt-4 to mt-2 */}
+            <p className="body-text-bold mt-2 text-black text-left">
+                Status: <span className="body-text">Not started yet</span>
+            </p>
+            <button className="expand-btn body-text-bold mt-2 text-black text-center">[ expand ]</button>
         </div>
     </div>
 );
